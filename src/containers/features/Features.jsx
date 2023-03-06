5c73a0bc301efe12b7c39968eba4f00d6c804ef9
oguegbe franklin <stonecode0047@gmail.com>
2023-03-06 07:12:17 +0100

    Updated website

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,5 +1,27 @@
-
+import { Feature } from "../../components/export";
 import "./features.css";
+
+const featuresData = [
+  {
+    title: "Improving end distrusts instantly ",
+    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+  },
+
+  {
+    title: "Become the tended active",
+    text: " Led ask possible mistress relation elegance eat likewise debating. By message or am nothing amongst chiefly address.",
+  },
+
+  {
+    title: "Message or am nothing",
+    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+  },
+
+  {
+    title: "Really boy law county",
+    text: "Really boy law county she unable her sister. Feet you off its like like six. Among sex are leave law built now. In built table in an rapid blush.",
+  },
+];
 
 const Features = () => {
   return (
@@ -13,10 +35,12 @@ const Features = () => {
       </div>
 
       <div className="gpt3__features-container">
-      
+        {featuresData.map((item, index) => (
+          <Feature title={item.title} text={item.text} key={index} />
+        ))}
       </div>
     </div>
   );
-}
+};
 
-export default Features
+export default Features;
